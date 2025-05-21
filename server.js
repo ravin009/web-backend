046b5c2c820ac643daa76015acfd8a74645a59c0
RAVI GANGWAR <kingravin001@gmail.com>
2025-05-22 01:32:48 +0530

    web-server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = process.env.PORT || 
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
